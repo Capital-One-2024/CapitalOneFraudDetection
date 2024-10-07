7a407798dd8f685e2cb9a909897b57b6a3703f7c
Benjamin Miller <benhmiller22@gmail.com>
2024-10-07 13:54:24 -0500

    Added route to OTP page from App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
+import OtpVerificationPage from "./pages/OtpVerificationPage";
 import SignUpPage from "./pages/SignUpPage";
+
 function App() {
     return (
         <Router>
@@ -9,6 +11,7 @@ function App() {
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/" element={<LoginPage />} />
                 <Route path="/signup" element={<SignUpPage />} />
+                <Route path="/OTP" element={<OtpVerificationPage />} />
             </Routes>
         </Router>
     );
