22a92355ab23668d56d99ead709c04f424ffe0f0
Mihir Trivedi <mihirtrivedi45@gmail.com>
2024-10-07 01:58:09 -0500

    SCRUM 3 implemented user signup page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/login" element={<LoginPage />} />
-
                 <Route path="/" element={<LoginPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
             </Routes>
         </Router>
     );
