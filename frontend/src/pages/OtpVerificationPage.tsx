1c847943d89b510fc84f7fa65862fbca15bf399c
Benjamin Miller <benhmiller22@gmail.com>
2024-10-07 00:36:51 -0500

    feat: Improved input flow and set up back and continue buttons for click behavior

--- frontend/src/pages/OtpVerificationPage.tsx
+++ frontend/src/pages/OtpVerificationPage.tsx
@@ -1,8 +1,8 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Grid2, Button, Typography } from '@mui/material';
+import { Grid2, Button, Typography, Link } from '@mui/material';
 import logo from '../assets/C1_Logo_White.png';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
@@ -33,6 +33,8 @@ const OTPInputWithZod = () => {
 
     const inputRefs = useRef<HTMLInputElement[]>([]);
 
+    const [focusIndex, setFocusIndex] = useState<number | null>(null);
+
     // Automatically focus the first input when the component mounts
     useEffect(() => {
         inputRefs.current[0]?.focus();
@@ -51,8 +53,15 @@ const OTPInputWithZod = () => {
 
     // Function to handle key events
     const handleKeyUp = (e: React.KeyboardEvent<HTMLInputElement>, index: number) => {
+        // If no value and backspace pressed, focus previous cell
         if (e.key === 'Backspace' && !e.currentTarget.value) {
-            // If the current input is empty and backspace is pressed, focus the previous input
+            if (index > 0) {
+                inputRefs.current[index - 1]?.focus();
+            }
+        }
+
+        // If value, remove and focus previous cell
+        if (e.key === 'Backspace' && e.currentTarget.value) {
             setValue(`otp${index}` as keyof OTPFormData, '');
             if (index > 0) {
                 inputRefs.current[index - 1]?.focus();
@@ -61,7 +70,7 @@ const OTPInputWithZod = () => {
     };
 
     // Function to handle pasting values
-    const handlePaste = (e: React.ClipboardEvent<HTMLInputElement>, index: number) => {
+    const handlePaste = (e: React.ClipboardEvent<HTMLInputElement>, _index: number) => {
         const pastedData = e.clipboardData.getData('text');
 
         // Only process if the pasted data is 6 digits long
@@ -81,7 +90,16 @@ const OTPInputWithZod = () => {
     };
 
     const onSubmit = (data: OTPFormData) => {
-        console.log('OTP submitted: ', Object.values(data).join(''));
+        const otp = Object.values(data).join('');
+        console.log('OTP submitted: ', otp);
+
+        const regex = /^\d{6}$/;
+
+        // Additional validation
+        if (!regex.test(otp)) {
+            console.error('OTP must be 6 digits');
+            return; // Stop submission if OTP is invalid
+        }
     };
 
     // Watching the values to enable/disable the submit button
@@ -102,7 +120,8 @@ const OTPInputWithZod = () => {
             }}
         >
             {/* Back Button Positioned Absolutely in Top Right Corner */}
-            <Button
+            <Link
+                href="/TODO"
                 sx={{
                     color: 'white',
                     position: 'absolute',
@@ -112,23 +131,26 @@ const OTPInputWithZod = () => {
                     alignItems: 'center', // Center vertically
                     textTransform: 'none',
                     fontSize: '2.5vh',
+                    textDecoration: 'none',
                     transition: 'transform 0.3s ease', // Smooth transition for scaling
                     ':hover': {
                         transform: 'scale(1.05)', // Scale on hover
-                        background: 'none'
+                        background: 'none',
+                        color: 'white',
+                        textDecoration: 'none',
                     }
                 }}
             >
                 <ArrowBackIcon sx={{ mr: 1 }} fontSize='small' /> {/* Add margin to the right of the icon */}
                 Back
-            </Button>
-            <Grid2 alignItems="stretch" sx={{ textAlign: 'center', mb: 8 }}>
+            </Link>
+            <Grid2 sx={{ textAlign: 'center', mb: 12 }}>
                 <Grid2>
                     <img
                         src={logo} // Use the imported image
-                        alt="Capital One Logo" // Add alt text for accessibility
+                        alt="Capital One Logo"
                         style={{
-                            width: '200px', // Set width
+                            width: '200px',
                             height: 'auto', // Maintain aspect ratio
                         }}
                     />
@@ -148,76 +170,104 @@ const OTPInputWithZod = () => {
                         Enter the code sent to your phone below.
                     </Typography>
                 </Grid2>
-                <Grid2 sx={{ my: 4, mb: 8 }}>
-                    <form onSubmit={handleSubmit(onSubmit)} style={{ display: 'flex', justifyContent: 'center', gap: '20px' }}>
-                        {[0, 1, 2, 3, 4, 5].map((index) => (
-                            <Controller
-                                key={index}
-                                name={`otp${index}` as keyof OTPFormData}
-                                control={control}
-                                render={({ field }) => (
-                                    <div>
-                                        <input
-                                            {...field}
-                                            type="text"
-                                            maxLength={1}
-                                            onChange={(e) => handleInputChange(e.target.value, index)}
-                                            onKeyUp={(e) => handleKeyUp(e, index)}
-                                            onPaste={(e) => handlePaste(e, index)}
-                                            ref={(el) => (inputRefs.current[index] = el as HTMLInputElement)}
-                                            style={{
-                                                width: '70px',
-                                                height: '70px',
-                                                textAlign: 'center',
-                                                fontSize: '30px',
-                                                border: '1px solid #ccc',
-                                                color: '#004878',
-                                                fontWeight: 600,
-                                                borderRadius: '10px',
-                                                caretColor: 'transparent', // Hide the cursor
-                                            }}
-                                        />
-                                    </div>
-                                )}
-                            />
-                        ))}
-                    </form>
-                </Grid2>
-                <Grid2 >
-                    <Button type="submit" disabled={!isFilled} variant='outlined' sx={{
-                        background: 'white',
-                        borderRadius: '10px',
-                        px: 4,
-                        color: '#004878',
-                        textTransform: 'none',
-                        width: '90%',
-                        boxShadow: !isFilled ? 'none' : '0 0 10px rgba(255, 255, 255, 0.5)',
-                        fontSize: '2.5vh',
-                        fontWeight: 600,
-                        transition: 'box-shadow 0.3s ease, background 0.3s ease, color 0.3s ease',
-                        ':hover': {
-                            border: '1.5px solid white',
-                            color: '#004878',
+
+                {/* Form with submit button */}
+                <form onSubmit={handleSubmit(onSubmit)} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '16px' }}>
+                    <Grid2 sx={{ my: 4 }}>
+                        <Grid2 container justifyContent="center" gap="2vw">
+                            {[0, 1, 2, 3, 4, 5].map((index) => (
+                                <Controller
+                                    key={index}
+                                    name={`otp${index}` as keyof OTPFormData}
+                                    control={control}
+                                    render={({ field }) => (
+                                        <div>
+                                            <input
+                                                {...field}
+                                                type="text"
+                                                maxLength={1}
+                                                onChange={(e) => handleInputChange(e.target.value, index)}
+                                                onKeyUp={(e) => handleKeyUp(e, index)}
+                                                onPaste={(e) => handlePaste(e, index)}
+                                                ref={(el) => (inputRefs.current[index] = el as HTMLInputElement)}
+                                                onFocus={() => setFocusIndex(index)} // Set the focus index on focus
+                                                onBlur={() => setFocusIndex(null)} // Reset focus index on blur
+                                                style={{
+                                                    width: '10vw', // Use viewport width to make it responsive
+                                                    maxWidth: '70px', // Set max width for larger screens
+                                                    minWidth: '50px', // Set min width for smaller screens
+                                                    height: '10vw',
+                                                    maxHeight: '70px',
+                                                    minHeight: '50px',
+                                                    textAlign: 'center',
+                                                    fontSize: '30px',
+                                                    backgroundColor: '#FFFFFF',
+                                                    color: '#004878',
+                                                    fontWeight: 600,
+                                                    borderRadius: '10px',
+                                                    caretColor: 'transparent', // Hide the cursor
+                                                    border: 'none',
+                                                    outline: focusIndex === index ? '3px solid rgba(255, 0, 0, 0.7)' : 'none', // Remove default outline
+                                                }}
+                                            />
+                                        </div>
+                                    )}
+                                />
+                            ))}
+                        </Grid2>
+                    </Grid2>
+                    <Grid2 sx={{ mt: 2, width: '100%' }}>
+                        <Button type="submit" disabled={!isFilled} variant='outlined' sx={{
                             background: 'white',
-                            boxShadow: '0 0 20px rgba(255, 255, 255, 0.7)',
-                        },
-                        // Styles for the disabled state
-                        '&.Mui-disabled': {
-                            background: 'rgba(255, 255, 255, 0.2)',
-                            color: 'rgba(255, 255, 255, 0.5)', 
-                            cursor: 'not-allowed', 
-                            boxShadow: 'none',
-                        },
-                    }}>
-                        Continue
-                    </Button>
-                </Grid2>
+                            borderRadius: '10px',
+                            px: 4,
+                            border: 'none',
+                            color: '#004878',
+                            textTransform: 'none',
+                            width: '90%',
+                            boxShadow: !isFilled ? 'none' : '0 0 10px rgba(255, 255, 255, 0.5)',
+                            fontSize: '2.5vh',
+                            fontWeight: 600,
+                            transition: 'box-shadow 0.3s ease, background 0.3s ease, color 0.3s ease',
+                            ':hover': {
+                                color: '#004878',
+                                background: 'white',
+                                boxShadow: '0 0 20px rgba(255, 255, 255, 0.7)',
+                            },
+                            ':focus': {
+                                outline: 'none',
+                            },
+                            // Styles for the disabled state
+                            '&.Mui-disabled': {
+                                background: 'rgba(255, 255, 255, 0.2)',
+                                color: 'rgba(255, 255, 255, 0.5)',
+                                cursor: 'not-allowed',
+                                boxShadow: 'none',
+                            },
+                        }}>
+                            Continue
+                        </Button>
+                    </Grid2>
+                </form>
+
+                {/* Resend code text */}
                 <Grid2 sx={{ mt: 2 }}>
                     <Typography sx={{
                         color: 'white',
                         fontSize: '2vh',
                     }}>
-                        Didn't receive a code? Resend.
+                        Didn't receive a code?{' '}
+                        <Typography
+                            component="span"
+                            onClick={() => console.log("Resend Code!")}
+                            sx={{
+                                cursor: 'pointer',
+                                textDecoration: 'underline',
+                                color: '#fff',
+                            }}
+                        >
+                            Click to resend.
+                        </Typography>
                     </Typography>
                 </Grid2>
             </Grid2>
