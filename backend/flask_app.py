67193c6c501abfd5559f0754c4798822b328b5b1
Benjamin Miller <benhmiller22@gmail.com>
2024-10-27 14:06:05 -0500

    Clean flask skeleton

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,22 +6,39 @@ app = Flask(__name__)
 def home():
     return 'Hello, World!'
 
-@app.route('/signup', methods=['POST'])
+@app.post('/signup')
 def signup():
     data = request.get_json()
 
     # Data Validation and Extraction
     if not data:
-        return jsonify({"error": "No data provided"}), 400     
+        return {"error": "No data provided"}, 400     
     if 'firstName' not in data:
-        return jsonify({"error": "First name is required"}), 400
+        return {"error": "First name is required"}, 400
     if 'lastName' not in data:
-        return jsonify({"error": "Last name is required"}), 400
+        return {"error": "Last name is required"}, 400
     if 'email' not in data:
-        return jsonify({"error": "Email is required"}), 400
+        return {"error": "Email is required"}, 400
 
-    # Database logic to create user
-    return jsonify(data)
+    # USer creation in database
+    if True: # Success
+        return {"message": "Registration successful!"}, 200
+    else: # Error
+        return {"error": "Unable to create user"}, 500
+
+@app.post('/login')
+def login():
+    data = request.get_json()
+
+    # Validate credentials (dummy validation for example)
+    if True:
+        return {"message": "Login successful!"}, 200
+    else:
+        return {"error": "Invalid login"}, 401
+
+@app.post('/logout')
+def logout():
+    data = request.get_json()
 
 if __name__ == '__main__':
     app.run(debug=True)
