fd842d65510a39efcd4afadc0ddb157670e8c67e
Joseph Frazier <josephfrazier@Josephs-MacBook-Pro-7.local>
2024-11-15 23:20:03 -0500

    initial commit for dashboard

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,13 @@
+
+import Page from "../components/Page";
+import Dashboard from "../components/Dashboard";
+
+function DashboardPage() {
+    return (
+        <Page title="Dashboard">
+            <Dashboard />
+        </Page>
+    );
+}
+
+export default DashboardPage;
