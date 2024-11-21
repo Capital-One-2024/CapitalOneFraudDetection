02fcbe1d109d541dbfcd239ee9ea17d73e3e4d21
Mostafa Ali <123837462+Moe-Ali1@users.noreply.github.com>
2024-11-20 19:38:18 -0600

    Fix Navbar styling to match Figma design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,66 @@
 import classNames from "classnames";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { SITE_MAP } from "../lib/constants";
+import { signOut } from "aws-amplify/auth";
 
-export default function Navbar() {
+interface NavbarProps {
+    variant?: "loggedOut" | "loggedIn";
+}
+
+const NAV_LINKS = [
+    {
+        title: "Dashboard",
+        to: "/dashboard",
+    },
+    {
+        title: "New Transaction",
+        to: "/new-transaction",
+    },
+];
+
+export default function Navbar({ variant = "loggedOut" }: NavbarProps) {
     return (
         <nav
             className={classNames(
-                "flex justify-between items-center bg-c1-blue p-5 sticky z-50",
-                "w-full top-0 border-b-2 border-b-c1-blue"
+                "flex justify-between items-center sticky px-5 z-50",
+                "w-full top-0 border-b-4 border-b-c1-blue bg-white"
             )}
         >
-            <Link to="/" className="text-2xl font-semibold text-white">
-                Capital One
-            </Link>
-            <div className="flex gap-4">
-                <Link to={SITE_MAP.auth({ initialAction: "signIn" })} className="btn btn-white">
-                    Sign In
+            <div className="flex items-center gap-4">
+                <Link to="/">
+                    <img src="/logo.svg" alt="Capital One" className="h-20 w-40" />
                 </Link>
+                {variant === "loggedIn" && (
+                    <>
+                        <div className="bg-c1-blue w-1 h-10 rounded-lg"></div>
+                        <div className="flex items-center gap-5">
+                            {NAV_LINKS.map((link) => (
+                                <NavLink
+                                    key={link.to}
+                                    to={link.to}
+                                    className={({ isActive }) =>
+                                        classNames("text-gray-500 font-medium text-base", {
+                                            "!font-bold !text-c1-blue": isActive,
+                                        })
+                                    }
+                                >
+                                    {link.title}
+                                </NavLink>
+                            ))}
+                        </div>
+                    </>
+                )}
+            </div>
+            <div className="flex">
+                {variant === "loggedIn" ? (
+                    <button type="button" className="btn btn-blue" onClick={() => signOut()}>
+                        Sign Out
+                    </button>
+                ) : (
+                    <Link to={SITE_MAP.auth({ initialAction: "signIn" })} className="btn btn-blue">
+                        Sign In
+                    </Link>
+                )}
             </div>
         </nav>
     );
